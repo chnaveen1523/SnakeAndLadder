b3d29123449939fffb0f3d7ba980cf297aac1f77
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-06 22:40:13 +0530

    [Naveen] Added a code

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -6,17 +6,43 @@ namespace SnakeLadderProblem_CS
 /// </summary>
     class SnakeLadder
     {
+        //UC3 snake ladder problem 
         //Generating a random no using Random method()
-        public static void CheckDice()
+        public static int CheckDice()
         {
             Random random = new Random();
             int CheckDice = random.Next(1, 6);
             Console.WriteLine("Dice number:" + CheckDice);
+            return CheckDice;
+        }
+        public static void CheckOption()
+        {
+            Random random = new Random();
+            int CheckOption = random.Next(0, 3);
+            int position = 0;
+            int dice = SnakeLadder.CheckDice();
+            //switch selection statement
+            switch (CheckOption)
+            {
+                case 0:
+                    Console.WriteLine("No play");
+                    break;
+                case 1:
+                    Console.WriteLine("Snake");
+                    position -= dice;
+                    Console.WriteLine("Player current position" + position);
+                    break;
+                case 2:
+                    Console.WriteLine("Ladder");
+                    position += dice;
+                    Console.WriteLine("Player current position" + position);
+                    break;
+            }
         }
 
         static void Main(string[] args)
         {
-            SnakeLadder.CheckDice();
+            SnakeLadder.CheckOption();
             Console.ReadLine();
         }
     }
