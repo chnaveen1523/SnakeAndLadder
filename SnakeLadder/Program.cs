4bc86d8f929a8e921e5ec0c5cfa5040788c1f217
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-06 22:38:29 +0530

    [Naveen] Added a code

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 
 namespace SnakeLadderProblem_CS
-{
+{/// <summary>
+/// Template for Snake ladder game problem
+/// </summary>
     class SnakeLadder
     {
-        public static void Position()
+        //Generating a random no using Random method()
+        public static void CheckDice()
         {
-            int position = 0;
-            Console.WriteLine("Player is at start position:" + position);
+            Random random = new Random();
+            int CheckDice = random.Next(1, 6);
+            Console.WriteLine("Dice number:" + CheckDice);
         }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Single player");
-            SnakeLadder.Position();
+            SnakeLadder.CheckDice();
             Console.ReadLine();
         }
     }
